d309fe96a9932145cf51cdd3cfaf96083c4f300e
SunTechniX <suntechnic@mail.ru>
2026-05-04 10:40:25 +0400

    Initial commit

---
+++ data/api_user_data.py
@@ -0,0 +1,11 @@
+BOOKING_DATA = {
+    "firstname": "Гриша",
+    "lastname": "Сазонов",
+    "totalprice": 2700,
+    "depositpaid": True,
+    "bookingdates": {
+        "checkin": "2026-05-04",
+        "checkout": "2026-06-11"
+    },
+    "additionalneeds": "Breakfast"
+}
